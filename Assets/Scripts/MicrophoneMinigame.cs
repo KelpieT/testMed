765946c829cffb14359214a6d9939b123df01275
Andrey <andrey@entailegames.ru>
2020-03-13 15:02:36 +0300

    cleanup code

--- Assets/Scripts/MicrophoneMinigame.cs
+++ Assets/Scripts/MicrophoneMinigame.cs
@@ -5,54 +5,50 @@ using UnityEngine.UI;
 
 public class MicrophoneMinigame : MonoBehaviour
 {
-
-    private int timeForOneClip = 100;
+    private int timeForOneClip = 100;//Time in seconds
     private AudioClip audioClip;
     private int frequancy = 44100;
-    public Slider targetSlider;
-    public Slider ValueSlider;
-    private float curVel;
-    public float smTime;
-    public float sliderSpeed;
-    public GameObject windowOk;
+    public Slider TargetSlider;//Assign in editor
+    public Slider ValueSlider;//Assign in editor
+    private float curVel;//For SmoothDamp slider value
+    public float smTime;//Assign in editor | For SmoothDamp slider value
+    public float sliderSpeed;//Assign in editor | For SmoothDamp slider value
+    public GameObject windowOk;//Assign in editor
     private bool windowOpen = false;
-    int test = 0;
+
     private void Start()
     {
-
-        audioClip = Microphone.Start(Microphone.devices[0], true, timeForOneClip, frequancy);
-
+        StartRecordAudio();
     }
     void Update()
     {
         SetValueToSlider();
-
+    }
+    private void StartRecordAudio()
+    {
+        audioClip = Microphone.Start(Microphone.devices[0], true, timeForOneClip, frequancy);
     }
     private void SetValueToSlider()
     {
         ValueSlider.value = Mathf.SmoothDamp(ValueSlider.value, GetValue(audioClip), ref curVel, smTime, sliderSpeed);
-        if (targetSlider.value < ValueSlider.value && !windowOpen )
+        if (TargetSlider.value > ValueSlider.value)
+        {
+            windowOpen = false;
+        }
+        else if (!windowOpen)
         {
             windowOpen = true;
             windowOk.SetActive(true);
             Invoke("DisableWindow", 1f);
         }
-        if(targetSlider.value > ValueSlider.value)
-        {
-            windowOpen = false;
-        }
-
 
     }
     private float GetValue(AudioClip audioClip)
     {
-
         int dec = 128;
         float[] waveData = new float[dec];
         int micPosition = Microphone.GetPosition(Microphone.devices[0]) - (dec + audioClip.channels);
         audioClip.GetData(waveData, micPosition);
-
-
         float levelMax = 0;
         for (int i = 0; i < dec; i++)
         {
@@ -62,12 +58,10 @@ public class MicrophoneMinigame : MonoBehaviour
                 levelMax = wavePeak;
             }
         }
-
         return Mathf.Sqrt(levelMax);
     }
-    void DisableWindow()
+    private void DisableWindow()//Use for Invoke
     {
         windowOk.SetActive(false);
-
     }
 }
